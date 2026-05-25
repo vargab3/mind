4b359cf6d2ca706fe9ad20f7ba34dca061c2b37d
vargab3 <146070587+vargab3@users.noreply.github.com>
2026-05-25 22:56:35 +0200

    Add files via upload

---
+++ celloveszetCLI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace celloveszetCLI_1
+{
+    public class Program
+    {
+        static List<cellovo> list = new List<cellovo>();
+        static void Main(string[] args)
+        {
+            string[] osszes = File.ReadAllLines("lovesek.csv");
+
+            foreach (var item in osszes)
+            {
+                list.Add(new cellovo(item));
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 9. Feladat
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.nev} + {item.legnagyobb()}");
+            }
+
+            //10. Feladat
+
+            cellovo max = list[0];
+
+            foreach (var item in list)
+            {
+                if (item.legnagyobb() > max.legnagyobb())
+                {
+                    max = item;
+                }
+            }
+            Console.WriteLine(max);
+
+            //11. Feladat
+            cellovo min_atlag = list[0];
+
+            foreach (var item in list)
+            {
+                if (item.atlag() < min_atlag.atlag())
+                {
+                    min_atlag = item;
+                }
+            }
+            Console.WriteLine(min_atlag);
+        }
+    }
+}
---
+++ celloveszetCLI/cellovo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celloveszetCLI_1
+{
+    public class cellovo
+    {
+
+        public string nev { get; set; }
+        public int elsoloves { get; set; }
+        public int masodikloves { get; set; }
+        public int harmadikloves { get; set; }
+        public int negyedikloves { get; set; }
+
+        public cellovo(string line)
+        {
+            string[] darabok = line.Split(";");
+
+            this.nev = darabok[0];
+            this.elsoloves = Convert.ToInt32(darabok[1]);
+            this.masodikloves = Convert.ToInt32(darabok[2]);
+            this.harmadikloves = Convert.ToInt32(darabok[3]);
+            this.negyedikloves = Convert.ToInt32(darabok[4]);
+        }
+
+        public cellovo(string nev, int elsoloves, int masodikloves, int harmadikloves, int negyedikloves)
+        {
+            this.nev = nev;
+            this.elsoloves = elsoloves;
+            this.masodikloves = masodikloves;
+            this.harmadikloves = harmadikloves;
+            this.negyedikloves = negyedikloves;
+        }
+
+        public override string ToString()
+        {
+            return nev + " " + elsoloves + " " + masodikloves + " " + harmadikloves + " " + negyedikloves;
+        }
+
+
+        public int legnagyobb()
+        {
+            List<int> lovesek = new List<int>();
+
+            lovesek.Add(elsoloves);
+            lovesek.Add(masodikloves);
+            lovesek.Add(harmadikloves);
+            lovesek.Add(negyedikloves);
+
+            return lovesek.Max();
+        }
+
+        public double atlag()
+        {
+            return (elsoloves + masodikloves + harmadikloves + negyedikloves) / 4;
+        }
+    }
+}
